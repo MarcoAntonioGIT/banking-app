3918228e46004a7d7e28018142f724b159267344
MarcoAntonioGIT <marcoavhu@gmail.com>
2024-11-01 17:53:09 -0300

    Gráfico com saldo de cada conta bancária

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+    const loggedIn = { firstName: 'Marco'};
+
     return(
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header"> 
+                    <HeaderBox 
+                        type="greeting"
+                        title="Bem vindo"
+                        user={ loggedIn?.firstName || "Guest"}
+                        subtext="Acesse a gerencie sua conta e transações com eficiência."
+                    />
+                    <TotalBalanceBox 
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.00}
+                    
+                />
+                </header>
+            </div>
+        </section>
     )
 }
 
